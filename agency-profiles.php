c4a681e1eae1c5b33cbf0984532d25dbad8c6f65
Samuel Huth <sam@stafflink.com.au>
2021-09-07 10:02:50 +1000

    save to table functionality

--- agency-profiles.php
+++ agency-profiles.php
@@ -59,6 +59,18 @@ function agency_profiles_activate(){
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($sql);
         add_option('my_db_version', $my_products_db_version);
+
+        // Set a default empty row for updating
+        $data = array(
+            'acf_group' => "",
+            'allowed_domains'    => ""
+        );
+        $format = array(
+            '%s',
+            '%s'
+        );
+        $success = $wpdb->insert( $table_name, $data, $format );
+
     }
 
 }
@@ -69,9 +81,13 @@ function agency_profiles_activate(){
 // ========================================================
 register_deactivation_hook( __FILE__, 'agency_profiles_deactivate' );
 
-function aagency_profiles_deactivate(){
+function agency_profiles_deactivate(){
     
     // Do anything?
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'agency_profiles';
+    $sql = "DROP TABLE IF EXISTS $table_name";
+    $wpdb->query($sql);
 
 }
 
@@ -88,5 +104,111 @@ function agency_profiles_uninstall(){
     $table_name = $wpdb->prefix . 'agency_profiles';
     $sql = "DROP TABLE IF EXISTS $table_name";
     $wpdb->query($sql);
+    
+}
+
+
+// ========================================================
+// Add Menu Option
+// ========================================================
+function my_admin_menu() {
+    add_menu_page(
+        __( 'Agency Profiles', 'agency-profiles' ),
+        __( 'Agency Profiles', 'agency-profiles' ),
+        'manage_options',
+        'admin-page',
+        'my_admin_page_contents',
+        'dashicons-groups',
+        100
+    );
+}
+
+add_action( 'admin_menu', 'my_admin_menu' );
+
+
+// ========================================================
+// Create Admin Page
+// ========================================================
+function my_admin_page_contents() {
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'agency_profiles';
+
+    $result = $wpdb->get_results( $wpdb->prepare("SELECT * FROM $table_name WHERE ID = 1"));
+
+    $acf_group = "";
+    $allowed_domains = "";
+    
+    if($result){
+        $acf_group = $result[0]->acf_group;
+        $allowed_domains = $result[0]->allowed_domains;
+
+    }
+
+    ?>
+    <h1>Agency Profiles</h1>
+    <form method="POST" action="<?php echo admin_url( 'admin.php' ); ?>">
+        
+        <br />
+        <label for="acf_group"><b>ACF Group ID</b></label>
+        <br />
+        <p><i>e.g. group_6136a4705950f</i></p>
+        <input type="text" value="<?= $acf_group; ?>" name="acf_group" id="acf_group">
+        <br />
+        <br />
+        <br />
+        <label for="allowed_domains"><b>Allowed Email Domains</b></label>
+        <br />
+        <p>Comma Separated list of domains.</p>
+        <p><i>e.g. stafflink.com.au, google.com.au</i></p>
+        <input type="text" value="<?= $allowed_domains; ?>" name="allowed_domains" id="allowed_domains">
+        <br />
+        <br />
+        <input type="hidden" name="action" value="save_agency_profile_admin" />
+        <input type="submit" value="Save Settings" />
+    </form>
+    <?php
+}
+
+
+// ========================================================
+// Save the Results to the DB
+// ========================================================
+add_action( 'admin_action_save_agency_profile_admin', 'admin_save_action' );
+function admin_save_action()
+{
+
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'agency_profiles';
+    $data = array(
+        'acf_group' => $_POST['acf_group'],
+        'allowed_domains'    => $_POST['allowed_domains']
+    );
+
+    // echo "UPDATE $table_name 
+    // SET acf_group = '". $data['acf_group'] ."',   
+    //     allowed_domains = '". $data['allowed_domains'] ."'  
+    // WHERE ID = 1"; exit;
+
+    $success = $wpdb->query( $wpdb->prepare("
+        UPDATE $table_name 
+        SET acf_group = '". $data['acf_group'] ."',   
+            allowed_domains = '". $data['allowed_domains'] ."'  
+        WHERE ID = 1")
+    );
+
+
+    // $success=$wpdb->insert( $table, $data, $format );
+    if($success){
+
+        wp_redirect( $_SERVER['HTTP_REFERER'] );
+        exit();
+
+    } else {
+        
+        echo "An error occurred. Unable to save to the database table $table";
+        exit;
+    
+    }
 
 }
